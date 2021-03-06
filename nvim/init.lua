e1d5405bca56699482521f9811f508b0b641c1cf
Will Lee <myungjoon.lee@vanderbilt.edu>
2021-03-05 21:10:02 -0500

    Skip remote provider config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = " "
 vim.g.loaded_matchparen = 1
 
+-- Skip some remote provider loading
+vim.g.loaded_python_provider = 0
+vim.g.python_host_prog = '/usr/bin/python2'
+vim.g.python3_host_prog = '/usr/bin/python'
+vim.g.node_host_prog = '/usr/bin/neovim-node-host'
+
 if vim.fn.executable('rg') == 1 then
     vim.g.rg_derive_root='true'
 end
