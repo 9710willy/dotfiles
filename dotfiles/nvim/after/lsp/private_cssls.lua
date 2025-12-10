f6bd3f44ea66b16f0af19716e4d74de975190e4d
will.lee <will.lee@five9.com>
2025-12-10 11:46:37 -0500

    Add dotfiles/nvim/after/lsp/cssls.lua Add dotfiles/nvim/after/lsp/julials.lua Add dotfiles/nvim/after/lsp/ltex.lua Add dotfiles/nvim/after/lsp/lua_ls.lua Add dotfiles/nvim/after/lsp/pyright.lua Add dotfiles/nvim/after/lsp/rust_analyzer.lua Add dotfiles/nvim/after/lsp/texlab.lua

---
+++ dotfiles/nvim/after/lsp/private_cssls.lua
@@ -0,0 +1,4 @@
+-- Add 'sass' filetype (not in nvim-lspconfig default)
+return {
+  filetypes = { "css", "scss", "less", "sass" },
+}
---
+++ dotfiles/nvim/after/lsp/private_julials.lua
@@ -0,0 +1,13 @@
+return {
+  on_new_config = function(new_config, _)
+    local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
+    if vim.uv.fs_stat(julia) then
+      new_config.cmd[1] = julia
+    end
+  end,
+  settings = {
+    julia = {
+      format = { indent = 2 },
+    },
+  },
+}
