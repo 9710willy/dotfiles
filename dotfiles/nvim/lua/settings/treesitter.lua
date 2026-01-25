232a6ef263e29453b752bb695dfa7d68fa8ef706
9710willy <9710willy@users.noreply.github.com>
2026-01-24 20:29:41 -0500

    Add .config/karabiner/karabiner.json Update dotfiles/nvim/after/lsp/eslint.lua Update dotfiles/nvim/lua/plugins.lua Update dotfiles/nvim/lua/settings/cmp.lua Update dotfiles/nvim/lua/settings/telescope.lua Update dotfiles/nvim/lua/settings/treesitter.lua

--- dotfiles/nvim/lua/settings/treesitter.lua
+++ dotfiles/nvim/lua/settings/treesitter.lua
@@ -23,17 +23,7 @@ require('nvim-treesitter.configs').setup {
   },
   refactor = {
     smart_rename = { enable = true, keymaps = { smart_rename = 'grr' } },
-    highlight_definitions = {
-      enable = true,
-      max_file_lines = 1000,
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
-      end,
-    },
+    highlight_definitions = { enable = false },
   },
   textsubjects = {
     enable = true,
